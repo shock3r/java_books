bb361dcfd02a9b744751786ddb05da7320093bdb
shock3r <shock3r.brest@gmail.com>
2019-10-25 10:07:52 +0300

    add code that show reflection example from book (G.Shield Java 8 Full)

--- src/by/ignathick/reflection/Main.java
+++ src/by/ignathick/reflection/Main.java
@@ -1,8 +1,38 @@
 package by.ignathick.reflection;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnno {
+    String str();
+    int val();
+}
 public class Main {
+    @MyAnno(str = "Example of annotation", val = 100)
+
+    public static void myMeth () {
+
+        Main ob = new Main();
+
+        //get anotation from method
+        //and print they members
+
+        try{
+
+            Class<?> c = ob.getClass();
+            Method m = c.getMethod("myMeth");
+            MyAnno anno = m.getAnnotation(MyAnno.class);
+            System.out.println(anno.str() + " " + anno.val());
+
+        } catch (NoSuchMethodException exc) {
+            System.out.println("Cant find method");
+        }
+
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        myMeth();
     }
 }
